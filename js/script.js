18ca7a22271b45573510ee9f98e225e627398fca
grinanastasia <grin2008.96@mail.ru>
2020-11-23 11:56:52 +0300

    add timer

--- js/script.js
+++ js/script.js
@@ -34,4 +34,53 @@ window.addEventListener('DOMContentLoaded', function() {
         }
 
     });
+    // timer
+    let deadline = '2020-11-24';
+
+    function getTimeREmaning(endTime){
+        let t = Date.parse(deadline) - Date.parse(new Date()),
+            seconds = Math.floor((t/1000)%60), // Math.floor  округляет значение
+            minutes = Math.floor((t/1000/60)%60),
+            hours = Math.floor((t/(1000*60*60)));
+            // hours = Math.floor((t/1000/60)%24),
+            // days = Math.floor((t/(1000*60*60*24))); если нужен таймер на дни
+
+        return{
+            'total':t,
+            'hours': hours,
+            'minutes': minutes,
+            'seconds': seconds
+        };
+    }
+    function setCloack(id,endTime){
+        let timer = document.getElementById(id),
+            hours = timer.querySelector('.hours'),
+            minutes = timer.querySelector('.minutes'),
+            seconds = timer.querySelector('.seconds'),
+            timeInterval = setInterval(updateCloack,1000);
+        
+        function updateCloack(){
+            let t = getTimeREmaning(endTime);
+            function addZero(num){
+                if (num<=9){
+                    return '0' + num;
+                }else {
+                    return num;
+                }
+            }
+            hours.textContent = addZero(t.hours);
+            minutes.textContent = addZero(t.minutes);
+            seconds.textContent = addZero(t.seconds);
+
+            if(t.total<=0){
+                clearInterval(timeInterval);
+                hours.textContent = '00';
+                minutes.textContent = '00';
+                seconds.textContent = '00';
+            }
+
+        }
+
+    }
+    setCloack('timer',deadline);
 });
