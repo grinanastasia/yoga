fefbecfc1ff45699033580913fe6310a71bb3db7
grinanastasia <grin2008.96@mail.ru>
2021-01-26 19:36:51 +0300

    all about ES6

--- js/script.js
+++ js/script.js
@@ -1,86 +1,125 @@
-window.addEventListener('DOMContentLoaded', function() {
+window.addEventListener('DOMContentLoaded', function () {
 
-    'use strict';
-    let tab = document.querySelectorAll('.info-header-tab'),
-        info = document.querySelector('.info-header'),
-        tabContent = document.querySelectorAll('.info-tabcontent');
+            'use strict';
+            let tab = document.querySelectorAll('.info-header-tab'),
+                info = document.querySelector('.info-header'),
+                tabContent = document.querySelectorAll('.info-tabcontent');
 
-    function hideTabContent(a) {
-        for (let i = a; i < tabContent.length; i++) {
-            tabContent[i].classList.remove('show');
-            tabContent[i].classList.add('hide');
-        }
-    }
-
-    hideTabContent(1);
-
-    function showTabContent(b) {
-        if (tabContent[b].classList.contains('hide')) {
-            tabContent[b].classList.remove('hide');
-            tabContent[b].classList.add('show');
-        }
-    }
-
-    info.addEventListener('click', function(event) {
-        let target = event.target;
-        if (target && target.classList.contains('info-header-tab')) {
-            for(let i = 0; i < tab.length; i++) {
-                if (target == tab[i]) {
-                    hideTabContent(0);
-                    showTabContent(i);
-                    break;
+            function hideTabContent(a) {
+                for (let i = a; i < tabContent.length; i++) {
+                    tabContent[i].classList.remove('show');
+                    tabContent[i].classList.add('hide');
                 }
             }
-        }
 
-    });
-    // timer
-    let deadline = '2020-11-24';
+            hideTabContent(1);
 
-    function getTimeREmaning(endTime){
-        let t = Date.parse(deadline) - Date.parse(new Date()),
-            seconds = Math.floor((t/1000)%60), // Math.floor  округляет значение
-            minutes = Math.floor((t/1000/60)%60),
-            hours = Math.floor((t/(1000*60*60)));
-            // hours = Math.floor((t/1000/60)%24),
-            // days = Math.floor((t/(1000*60*60*24))); если нужен таймер на дни
-
-        return{
-            'total':t,
-            'hours': hours,
-            'minutes': minutes,
-            'seconds': seconds
-        };
-    }
-    function setCloack(id,endTime){
-        let timer = document.getElementById(id),
-            hours = timer.querySelector('.hours'),
-            minutes = timer.querySelector('.minutes'),
-            seconds = timer.querySelector('.seconds'),
-            timeInterval = setInterval(updateCloack,1000);
-        
-        function updateCloack(){
-            let t = getTimeREmaning(endTime);
-            function addZero(num){
-                if (num<=9){
-                    return '0' + num;
-                }else {
-                    return num;
+            function showTabContent(b) {
+                if (tabContent[b].classList.contains('hide')) {
+                    tabContent[b].classList.remove('hide');
+                    tabContent[b].classList.add('show');
                 }
             }
-            hours.textContent = addZero(t.hours);
-            minutes.textContent = addZero(t.minutes);
-            seconds.textContent = addZero(t.seconds);
 
-            if(t.total<=0){
-                clearInterval(timeInterval);
-                hours.textContent = '00';
-                minutes.textContent = '00';
-                seconds.textContent = '00';
+            info.addEventListener('click', function (event) {
+                let target = event.target;
+                if (target && target.classList.contains('info-header-tab')) {
+                    for (let i = 0; i < tab.length; i++) {
+                        if (target == tab[i]) {
+                            hideTabContent(0);
+                            showTabContent(i);
+                            break;
+                        }
+                    }
+                }
+
+            });
+            // timer
+            let deadline = '2021-01-24';
+
+            function getTimeREmaning(endTime) {
+                let t = Date.parse(deadline) - Date.parse(new Date()),
+                    seconds = Math.floor((t / 1000) % 60), // Math.floor  округляет значение
+                    minutes = Math.floor((t / 1000 / 60) % 60),
+                    hours = Math.floor((t / (1000 * 60 * 60)));
+                // hours = Math.floor((t/1000/60)%24),
+                // days = Math.floor((t/(1000*60*60*24))); если нужен таймер на дни
+
+                return {
+                    'total': t,
+                    'hours': hours,
+                    'minutes': minutes,
+                    'seconds': seconds
+                };
             }
 
-        }
+            function setCloack(id, endTime) {
+                let timer = document.getElementById(id),
+                    hours = timer.querySelector('.hours'),
+                    minutes = timer.querySelector('.minutes'),
+                    seconds = timer.querySelector('.seconds'),
+                    timeInterval = setInterval(updateCloack, 1000);
 
-    }
-    setCloack('timer',deadline);
-});
+                function updateCloack() {
+                    let t = getTimeREmaning(endTime);
+
+                    function addZero(num) {
+                        if (num <= 9) {
+                            return '0' + num;
+                        } else {
+                            return num;
+                        }
+                    }
+                    hours.textContent = addZero(t.hours);
+                    minutes.textContent = addZero(t.minutes);
+                    seconds.textContent = addZero(t.seconds);
+
+                    if (t.total <= 0) {
+                        clearInterval(timeInterval);
+                        hours.textContent = '00';
+                        minutes.textContent = '00';
+                        seconds.textContent = '00';
+                    }
+
+                }
+
+            }
+            setCloack('timer', deadline);
+
+
+            // modal widow
+
+
+            let more = document.querySelector('.more'),
+                overlay = document.querySelector('.overlay'),
+                close = document.querySelector('.popup-close'),
+                btnMore = document.querySelectorAll('.description-btn');
+
+            more.addEventListener('click', function () {
+                overlay.style.display = 'block';
+                this.classList.add('more-splash');
+                document.body.style.overflow = 'hidden'; // запрешает прокрутку страницы, пока открыто модальное окно
+            });
+            close.addEventListener('click', function () {
+                overlay.style.display = 'none';
+                more.classList.remove('more-splash');
+                document.body.style.overflow = '';
+            });
+
+            
+            for (let i = 0; i < btnMore.length; i++) {
+                btnMore[i].addEventListener('click', openW);    
+            } 
+            // btn.forEach(function(btn) {
+            //     btn.addEventListener('click', function() {
+            //         overlay.style.display = 'block';
+            //         this.classList.add('more-splash');
+            //         document.body.style.overflow = 'hidden';   ИЛИ ТАК!
+            //     });
+            // });
+            function openW() {
+                overlay.style.display = 'block';
+                this.classList.add('more-splash');
+                document.body.style.overflow = 'hidden';
+            }
+            });
